8b2073e00a9e91b30bfb97128b02e7f1c11c7264
Yudelka Torres Amparo <yudelka_torres@ucne.edu.do>
2025-10-27 18:57:11 -0400

    Creando el contexto.

---
+++ P2_AP1_YudelkaTorres/DAL/Contexto.cs
@@ -0,0 +1,10 @@
+﻿using P2_AP1_YudelkaTorres.Models;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace P2_AP1_YudelkaTorres.DAL;
+public class Contexto : DbContext
+{
+    public Contexto(DbContextOptions<Contexto> options) : base(options) { }
+    public DbSet<Registro> Registro { get; set; }
+}
