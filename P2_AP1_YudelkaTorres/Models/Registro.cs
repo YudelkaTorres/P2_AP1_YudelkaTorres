e94f870dcb9659c7607b5a7905c49c17831bf7b6
Yudelka Torres Amparo <yudelka_torres@ucne.edu.do>
2025-10-27 20:47:58 -0400

    Creando el modelo.

--- P2_AP1_YudelkaTorres/Models/Registro.cs
+++ P2_AP1_YudelkaTorres/Models/Registro.cs
@@ -4,5 +4,6 @@ namespace P2_AP1_YudelkaTorres.Models;
 public class Registro
 {
     [Key]
-    public int RegistroId { get; set; } 
+    public int RegistroId { get; set; }
+    public DateTime Fecha { get; set; } = DateTime.Now;
 }
