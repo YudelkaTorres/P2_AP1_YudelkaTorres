b3a992905e2930f3f721e024c4318a429a022dd7
Yudelka Torres Amparo <yudelka_torres@ucne.edu.do>
2025-10-27 20:46:30 -0400

    Creando el Service.

---
+++ P2_AP1_YudelkaTorres/Service/RegistroService.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using P2_AP1_YudelkaTorres.DAL;
+using P2_AP1_YudelkaTorres.Models;
+using System.Linq.Expressions;
+
+namespace P2_AP1_YudelkaTorres.Service;
+public class RegistroService(IDbContextFactory<Contexto> DbFactory)
+{
+    public async Task <List<Registro>> Listar (Expression<Func<Registro, bool>> criterio)
+    {
+        await using var contexto = await DbFactory.CreateDbContextAsync();
+        return await contexto.Registro
+            .Where(criterio)
+            .AsNoTracking()
+            .ToListAsync();
+    }
+}
