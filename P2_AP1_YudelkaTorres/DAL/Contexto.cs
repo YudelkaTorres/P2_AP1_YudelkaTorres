4299a18df2727f2cb35ab02db1dd9c5e3d8b9744
Yudelka Torres Amparo <yudelka_torres@ucne.edu.do>
2025-11-03 18:41:05 -0400

    Agregando componente.

--- P2_AP1_YudelkaTorres/DAL/Contexto.cs
+++ P2_AP1_YudelkaTorres/DAL/Contexto.cs
@@ -7,4 +7,34 @@ public class Contexto : DbContext
 {
     public Contexto(DbContextOptions<Contexto> options) : base(options) { }
     public DbSet<Registro> Registro { get; set; }
+    public DbSet<Componente> Componentes { get; set; }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.Entity<Componente>(entity =>
+        {
+            entity.HasData(
+                new Componente
+                {
+                    ComponenteId = 1,
+                    Descripcion = "Memoria 4GB",
+                    Precio = 1580,
+                    Existencia = 1
+                },
+                new Componente
+                {
+                    ComponenteId = 2,
+                    Descripcion = "Disco SSD 120MB",
+                    Precio = 4200,
+                    Existencia = 8
+                },
+                new Componente
+                {
+                    ComponenteId = 3,
+                    Descripcion = "Tarjeta de Video",
+                    Precio = 10000,
+                    Existencia = 4
+                });
+        });
+    }
 }
